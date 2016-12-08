8fe036d9151f09cf76096d44bf0436b93e20667f
florian peyron <flotos@hotmail.fr>
2016-12-08 20:57:14 +0000

    Lina 3 abilities completed

--- scripts/vscripts/abilities/lina/impale.lua
+++ scripts/vscripts/abilities/lina/impale.lua
@@ -6,11 +6,22 @@ function Impale( keys )
 	local caster = keys.caster
 	local ability = keys.ability
 	local damageDone = ability:GetAbilityDamage()
+
+	-- Search for the totem buff value
+	local totem_spell = caster:FindAbilityByName("lina_totem")
+	local totem_level = totem_spell:GetLevel() - 1
+	local totem_bonus_damage = totem_spell:GetLevelSpecialValueFor("bonus_damage", totem_level)
+
+	-- Applies it if Lina is under the totem aura
+	if caster:HasModifier("modifier_totem_aura") then
+		damageDone = damageDone * (1+ (totem_bonus_damage/100))
+	end
+
 	local damageTable = {
 		victim = target,
 		attacker = caster,
 		damage = damageDone,
-		damage_type = DAMAGE_TYPE_PURE,
+		damage_type = DAMAGE_TYPE_MAGICAL,
 	}
 	 
 	ApplyDamage(damageTable)
