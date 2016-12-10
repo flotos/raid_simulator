02f21a5f5f05d6a5cebc7b4c8d347c685c12fe5a
florian peyron <flotos@hotmail.fr>
2016-12-10 01:33:29 +0000

    Lina R working

--- scripts/vscripts/abilities/lina/totemic_slam.lua
+++ scripts/vscripts/abilities/lina/totemic_slam.lua
@@ -2,7 +2,7 @@
 	Date: 19.01.2015.
 	Deals damage target]]
 function TotemicSlam( keys )
-	require('../../libraries/timers')
+	-- require('../../libraries/timers')
 
 
 
@@ -21,15 +21,30 @@ function TotemicSlam( keys )
 	local target_teams = DOTA_UNIT_TARGET_TEAM_FRIENDLY
 	local target_types = DOTA_UNIT_TARGET_ALL
 	local target_flags = DOTA_UNIT_TARGET_FLAG_PLAYER_CONTROLLED
-	local units = FindUnitsInRadius(caster_team, caster_location, nil, 9000, target_teams, target_types, target_flags, FIND_CLOSEST, false)
+	local units = FindUnitsInRadius(caster_team, caster_location, nil, 500, target_teams, target_types, target_flags, FIND_CLOSEST, false)
 
+	local number_of_totems = 0
+	local totems = {}
 	for _,unit in ipairs(units) do
 		print("[RAID] Units detected around : ", unit:GetUnitLabel())
 		-- Check if the found unit is under the casters control
 		if unit:GetUnitLabel() == "npc_lina_totem" then
 			print("[RAID] Casting once on : ", unit:GetAbsOrigin())
 			-- caster:CastAbilityOnTarget(unit, lina_impale, player_id)
-			caster:CastAbilityOnPosition(unit:GetAbsOrigin(), lina_impale, player_id)
+			number_of_totems = number_of_totems + 1
+			totems[number_of_totems] = unit
 		end
 	end
+
+	Timers:CreateTimer( 0.1, function()
+		caster:CastAbilityOnPosition(totems[number_of_totems]:GetAbsOrigin(), lina_impale, player_id)
+		number_of_totems = number_of_totems - 1
+		if number_of_totems > 0 then
+			return 0
+		else
+			return nil
+		end
+	end )
+
+
 end
